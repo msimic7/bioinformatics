f098948001e3ec31024d44b255e6ac339289f7b2
msimic7 <simicmilan017@gmail.com>
2021-01-14 22:24:10 +0100

    Lesson added

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import copy from 'copy-to-clipboard';
 import classes from './CodeEditor.module.scss';
 
-const CodeEditor = ({ code, result, customClass }) => {
+const CodeEditor = ({ code, result, copyable, customClass }) => {
   const [isRun, setIsRun] = useState(false);
 
   const handleRun = () => {
@@ -16,30 +16,47 @@ const CodeEditor = ({ code, result, customClass }) => {
 
   return (
     <div className={`${classes.root} ${customClass}`}>
-      <pre>{code}</pre>
       <div className={classes.options}>
-        {result !== '' && (
-          <span aria-hidden="true" className={classes.codeActions} onClick={handleRun}>
-            <i className="fas fa-play" />
-          </span>
-        )}
-        <span aria-hidden="true" className={classes.codeActions} onClick={handleCopyToClipboard}>
-          <i className="far fa-copy" />
-        </span>
+        Code
+        <div className={classes.actions}>
+          {result !== '' && (
+            <span aria-hidden="true" className={classes.codeActions} onClick={handleRun}>
+              <i className="fas fa-play" />
+            </span>
+          )}
+          {copyable && (
+            <span
+              aria-hidden="true"
+              className={classes.codeActions}
+              onClick={handleCopyToClipboard}
+            >
+              <i className="far fa-copy" />
+            </span>
+          )}
+        </div>
       </div>
-      {isRun && result !== '' && <pre>{result}</pre>}
+      <pre className={classes.code}>{code}</pre>
+      {isRun && result !== '' && (
+        <div className={classes.console}>
+          <p>Console</p>
+          <pre>{result}</pre>
+        </div>
+      )}
     </div>
   );
 };
 
 CodeEditor.propTypes = {
   code: PropTypes.string.isRequired,
-  result: PropTypes.string.isRequired,
+  result: PropTypes.string,
+  copyable: PropTypes.bool,
   customClass: PropTypes.string,
 };
 
 CodeEditor.defaultProps = {
   customClass: '',
+  result: '',
+  copyable: true,
 };
 
 export default CodeEditor;
